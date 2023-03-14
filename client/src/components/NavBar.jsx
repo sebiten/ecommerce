b95638fd401674d7302da09499d2da27c0d5f233
Sebiten <sebdevspace@gmail.com>
2023-03-14 15:32:38 -0300

    nav

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,4 @@
 import {
-  // eslint-disable-line
   Box,
   Flex,
   HStack,
@@ -13,15 +12,38 @@ import {
   useColorModeValue,
   useColorMode,
 } from "@chakra-ui/react";
-import { Link as ReactLink } from "react-router-dom"; // eslint-disable-line
-import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons"; // eslint-disable-line
-import { GiTechnoHeart } from "react-icons/gi"; // eslint-disable-line
+import { Link as ReactLink } from "react-router-dom";
+import { HamburgerIcon, CloseIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
+import { GiTechnoHeart } from "react-icons/gi";
+import { color } from "framer-motion";
 
+const links = [
+  { linkname: "Products", path: "/products" },
+  { linkname: "ShoppingCart", path: "/cart" },
+];
+
+const NavLink = ({ path, children }) => (
+  <Link
+    fontWeight="normal"
+    as={ReactLink}
+    to={path}
+    px={2}
+    py={2}
+    rounded="md"
+    _hover={{
+      textDecoration: "none",
+      bg: useColorModeValue("gray.200", "gray.700"),
+    }}
+  >
+    {children}
+  </Link>
+);
 const NavBar = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure(); // eslint-disable-line
-  const { colorMode, toggleColorMode } = useColorMode(); // eslint-disable-line
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <>
+      <div></div>
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <IconButton
@@ -30,29 +52,37 @@ const NavBar = () => {
             display={{ md: "none" }}
             onClick={isOpen ? onClose : onOpen}
           />
-          <HStack spacing={8} alignItems={"center"}>
-            <HStack
-              as={"nav"}
-              spacing={4}
-              display={{ base: "none", md: "flex" }}
-            >
-              <Link as={ReactLink} to="/">
-                <Flex alignItems="center">
-                  <Icon as={GiTechnoHeart} w={8} h={8} color="orange.400" />
-                  <Text
-                    fontFamily={"sans-serif"}
-                    fontSize="xl"
-                    fontWeight="bold"
-                  >
-                    Tech Lines
-                  </Text>
-                </Flex>
-              </Link>
+          <HStack>
+            <Link as={ReactLink} to="/">
+              <Flex alignItems="center">
+                <Icon as={GiTechnoHeart} w={8} h={8} color="orange.400" />
+                <Text fontSize="xl" fontWeight="bold">
+                  Tech Lines
+                </Text>
+              </Flex>
+            </Link>
+            <HStack>
+              {links.map((link) => (
+                <NavLink key={link.linkname} path={link.path}>
+                  {link.linkname}
+                </NavLink>
+              ))}
             </HStack>
           </HStack>
+          <Flex alignItems={"center"}>
+            <NavLink>
+              <Icon
+                as={colorMode === "light" ? MoonIcon : SunIcon}
+                alignSelf="center"
+                justifySelf="center"
+                onClick={toggleColorMode}
+              />
+            </NavLink>
+          </Flex>
+          <button>Sign up</button>
+          <button>Register</button>
         </Flex>
       </Box>
-      <Box></Box>
     </>
   );
 };
