af9ca9dac2dad02a4f13ced9c7393b035361069a
Sebiten <sebdevspace@gmail.com>
2023-03-27 12:20:36 -0300

    ordersummery

--- client/src/components/CartOrderSummer.jsx
+++ client/src/components/CartOrderSummer.jsx
@@ -1,21 +1,79 @@
-import {Button, Flex, Heading, Stack, Text, useColorModeValue as mode, Badge } from '@chakra-ui/react'
-import { useState } from 'react'
-import { FaArrowRigth } from 'react-icons/fa'
-import { useSelector } from 'react-redux'
-import { Link as ReactLink, useNavigate } from 'react-router-dom'
-
-
+import {
+  Button,
+  Flex,
+  Heading,
+  Stack,
+  Text,
+  useColorModeValue as mode,
+  Badge,
+} from "@chakra-ui/react";
+import { useState } from "react";
+import { FaArrowRigth } from "react-icons/fa";
+import { useSelector } from "react-redux";
+import { Link as ReactLink, useNavigate } from "react-router-dom";
 
 const CartOrderSummer = () => {
-  const [buttonLoading, setButtonLoading] = useState()
+  const [buttonLoading, setButtonLoading] = useState();
   const standardShipping = Number(5.99).toFixed(2);
   const cartItems = useSelector((state) => state.cart.cart);
-  const {subtotal} = cartItems;
-  const navigate = useNavigate()
+  const { subtotal } = cartItems;
+  const navigate = useNavigate();
+
+  const checkoutHandler = () => {
+    setButtonLoading(true);
+    navigate('/checkout');
+  };
 
   return (
-    <div>CartOrderSummer</div>
-  )
-}
+    <Stack spacing="8" borderWidth="1px" rounded="lg" padding="8" w="full">
+      <Heading size="md">Order Summary</Heading>
+      <Stack spacing="6">
+        <Flex justify="space-between">
+          <Text fontWeight="medium" color={mode("gray.600", "gray.400")}>
+            Subtotal
+          </Text>
+          <Text fontWeight="medium">${subtotal}</Text>
+        </Flex>
+        <Flex justify="space-between">
+          <Text fontWeight="medium" color={mode("gray.600", "gray.400")}>
+            Shipping
+          </Text>
+          <Text fontWeight="medium">
+            {subtotal <= 1000 ? (
+              standardShipping
+            ) : (
+              <Badge rounded="full" px="2" fontSize="0.8em" colorScheme="green">
+                Free
+              </Badge>
+            )}
+          </Text>
+        </Flex>
+        <Flex justify="space-between">
+          <Text fontSize="xl" fontWeight="extrabold">
+            Total
+          </Text>
+          <Text fontSize="xl" fontWeight="extrabold">
+            ${" "}
+            {subtotal <= 1000
+              ? Number(subtotal) + Number(standardShipping)
+              : subtotal}
+          </Text>
+        </Flex>
+      </Stack>
+      <Button
+        as={ReactLink}
+        to="/checkout"
+        colorScheme="orange"
+        size="lg"
+        fontSize="md"
+        rightIcon={<FaArrowRight />}
+        isLoading={buttonLoading}
+        onClick={() => checkoutHandler()}
+      >
+        Checkout
+      </Button>
+    </Stack>
+  );
+};
 
-export default CartOrderSummer
+export default CartOrderSummer;
