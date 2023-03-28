8d2c39c7c95807e3766975031172679b5b5e3618
Sebiten <sebdevspace@gmail.com>
2023-03-28 11:48:48 -0300

    summerydone

--- client/src/redux/slices/cart.js
+++ client/src/redux/slices/cart.js
@@ -1,15 +1,26 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
+
+const calculateSubtotal = (cartState) => {
+  let result = 0;
+  cartState.map((item) => (result += item.qty * item.price));
+  return Number(result).toFixed(2);
+};
 
 export const initialState = {
   loading: false,
   error: null,
-  cart: [],
-  expressShipping: false,
-  subtotal: 0,
+  cart: JSON.parse(localStorage.getItem('cartItems')) ?? [],
+  expressShipping: JSON.parse(localStorage.getItem('expressShipping')) ?? false,
+  subtotal: localStorage.getItem('cartItems') ? calculateSubtotal(JSON.parse(localStorage.getItem('cartItems'))) : 0,
+};
+
+const updateLocalStorage = (cart) => {
+  localStorage.setItem('cartItems', JSON.stringify(cart));
+  localStorage.setItem('subtotal', JSON.stringify(calculateSubtotal(cart)));
 };
 
 export const cartSlice = createSlice({
-  name: "cart",
+  name: 'cart',
   initialState,
   reducers: {
     setLoading: (state) => {
@@ -17,24 +28,40 @@ export const cartSlice = createSlice({
     },
     cartItemAdd: (state, { payload }) => {
       const existingItem = state.cart.find((item) => item.id === payload.id);
+
       if (existingItem) {
-        state.cart = state.cart.map((item) =>
-          item.id === existingItem.id ? payload : item
-        );
+        state.cart = state.cart.map((item) => (item.id === existingItem.id ? payload : item));
       } else {
         state.cart = [...state.cart, payload];
       }
       state.loading = false;
       state.error = null;
+      updateLocalStorage(state.cart);
+      state.subtotal = calculateSubtotal(state.cart);
     },
     setError: (state, { payload }) => {
-      state.loading = false;
       state.error = payload;
+      state.loading = false;
+    },
+    cartItemRemoval: (state, { payload }) => {
+      state.cart = [...state.cart].filter((item) => item.id !== payload);
+      updateLocalStorage(state.cart);
+      state.subtotal = calculateSubtotal(state.cart);
+      state.loading = false;
+      state.error = null;
+    },
+    setExpressShipping: (state, { payload }) => {
+      state.expressShipping = payload;
+      localStorage.setItem('expressShipping', payload);
+    },
+    clearCart: (state) => {
+      localStorage.removeItem('cartItems');
+      state.cart = [];
     },
   },
 });
 
-export const { setLoading, cartItemAdd, setError } = cartSlice.actions;
+export const { setLoading, setError, cartItemAdd, cartItemRemoval, setExpressShipping, clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
 
 export const cartSelector = (state) => state.cart;
