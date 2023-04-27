6c27be7bc11cf70dd79ae9aa70c179ef10591d08
Sebiten <sebdevspace@gmail.com>
2023-04-27 13:24:15 -0300

    update profile

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { setLoading, setError, userLogin, userLogout } from "../slices/user.js";
+import { setLoading, setError, userLogin, userLogout, updateUserProfile, resetUpdate } from "../slices/user.js";
 
 export const login = (email, password) => async (dispatch) => {
   dispatch(setLoading(true));
@@ -62,4 +62,40 @@ export const register = (name, email, password) => async (dispatch) => {
       )
     );
   }
+};
+export const updateProfile =
+  (id, name, email, password) => async (dispatch, getState) => {
+    const {
+      user: { userInfo },
+    } = getState();
+
+    try {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${userInfo.token}`,
+          "Content-Type": "application/json",
+        },
+      };
+      const { data } = await axios.put(
+        `/api/users/profile/${id}`,
+        { _id: id, name, email, password },
+        config
+      );
+      localStorage.setItem("userInfo", JSON.stringify(data));
+      dispatch(updateUserProfile(data));
+    } catch (error) {
+      dispatch(
+        setError(
+          error.response && error.response.data
+            ? error.response.data
+            : error.message
+            ? error.message
+            : "An unexpected error has occured. Please try again later."
+        )
+      );
+    }
+  };
+
+export const resetUpdateSuccess = () => async (dispatch) => {
+  dispatch(resetUpdate());
 };
