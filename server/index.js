46de9ab18de9603bd533d08ed85408865a8081e1
Sebiten <sebdevspace@gmail.com>
2023-05-03 17:47:30 -0300

    order backend done

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import connectDB from "./database.js";
 // our routes
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/useRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 dotenv.config();
 connectDB();
@@ -13,6 +14,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
