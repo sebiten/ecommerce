e71b2ef18aa45d3c503b03b23efdcccb68542145
Sebiten <sebdevspace@gmail.com>
2023-05-17 17:23:32 -0300

    update-create-delete

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,10 +1,8 @@
 import express from "express";
 import Product from "../models/product.js";
-import asyncHandler from 'express-async-handler'
-import User from '../models/User.js'
-import {protectRoute} from "../middleware/authMiddleware.js";
-
-
+import asyncHandler from "express-async-handler";
+import User from "../models/User.js";
+import { protectRoute, admin} from "../middleware/authMiddleware.js";
 
 const productRoutes = express.Router();
 
@@ -32,11 +30,13 @@ const createProductReview = asyncHandler(async (req, res) => {
   const user = await User.findById(userId);
 
   if (product) {
-    const alreadyReviewed = product.reviews.find((rev) => rev.user.toString() === user._id.toString());
+    const alreadyReviewed = product.reviews.find(
+      (rev) => rev.user.toString() === user._id.toString()
+    );
 
     if (alreadyReviewed) {
       res.status(400);
-      throw new Error('Product already reviewed.');
+      throw new Error("Product already reviewed.");
     }
 
     const review = {
@@ -50,19 +50,89 @@ const createProductReview = asyncHandler(async (req, res) => {
     product.reviews.push(review);
 
     product.numberOfReviews = product.reviews.length;
-    product.rating = product.reviews.reduce((acc, item) => item.rating + acc, 0) / product.reviews.length;
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length;
     await product.save();
-    res.status(201).json({ message: 'Review has been saved.' });
+    res.status(201).json({ message: "Review has been saved." });
+  } else {
+    res.status(404);
+    throw new Error("Product not found.");
+  }
+});
+//create a product
+const createNewProduct = asyncHandler(async (req, res) => {
+  const {
+    brand,
+    name,
+    category,
+    stock,
+    price,
+    image,
+    productIsNew,
+    description,
+  } = req.body;
+
+  const newProduct = await Product.create({
+    brand,
+    name,
+    category,
+    stock,
+    price,
+    image: "/images/" + image,
+    productIsNew,
+    description,
+  });
+  await newProduct.save();
+
+  const products = await Product.find({});
+
+  if (newProduct) {
+    res.json(products);
+  } else {
+    res.status(404);
+    throw new Error("Product could not be uploaded.");
+  }
+});
+// delete a product
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findByIdAndDelete(req.params.id);
+
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
+//update a product
+const updateProduct = asyncHandler(async (req, res) => {
+  const { brand, name, image, category, stock, price, id, productIsNew, description } = req.body;
+
+  const product = await Product.findById(id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.brand = brand;
+    product.image = '/images/' + image;
+    product.category = category;
+    product.stock = stock;
+    product.productIsNew = productIsNew;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
   } else {
     res.status(404);
     throw new Error('Product not found.');
   }
 });
-
-
 productRoutes.route("/").get(getProducts);
 productRoutes.route("/:id").get(getProduct);
-productRoutes.route('/reviews/:id').post(protectRoute, createProductReview);
-
+productRoutes.route("/reviews/:id").post(protectRoute, createProductReview);
+productRoutes.route('/').put(protectRoute, admin, updateProduct);
+productRoutes.route('/:id').delete(protectRoute, admin, deleteProduct);
+productRoutes.route('/').post(protectRoute, admin, createNewProduct);
 
 export default productRoutes;
