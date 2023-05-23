d5b76d3a1077db6b5be791a57979f571d0def381
Sebiten <sebdevspace@gmail.com>
2023-05-23 12:45:58 -0300

    almost2

--- server/index.js
+++ server/index.js
@@ -1,35 +1,47 @@
 import dotenv from "dotenv";
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
-import userRoutes from "./routes/useRoutes.js";
-dotenv.config();
+import userRoutes from "./routes/userRoutes.js";
 import express from "express";
 import connectDB from "./database.js";
+import { createServer as createViteServer } from "vite";
 import path from "path";
-// our routes
 
-
+dotenv.config();
 connectDB();
-const app = express();
 
-app.use(express.json());
-app.use("/api/products", productRoutes);
-app.use("/api/users", userRoutes);
-app.use("/api/orders", orderRoutes);
+async function startServer() {
+  const app = express();
+  const port = process.env.PORT || 5000;
 
-app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
+  // Crear una instancia de servidor Vite en modo de desarrollo
+  const vite = await createViteServer({
+    server: { middlewareMode: true }
+  });
 
-const port = process.env.PORT || 5000;
+  app.use(express.json());
+  app.use("/api/products", productRoutes);
+  app.use("/api/users", userRoutes);
+  app.use("/api/orders", orderRoutes);
 
-const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+  app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID));
 
-if (process.env.NODE_ENV == 'production') {
-  app.use(express.static(path.join(__dirname, '/client/build')));
+  // Usar el middleware de Vite en Express
+  app.use(vite.middlewares);
 
-  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  // Configurar rutas estáticas y envío del archivo index.html
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client', 'dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+    });
+  }
+
+  // Iniciar el servidor de Express
+  app.listen(port, () => {
+    console.log(`Servidor Express iniciado en el puerto ${port}`);
+  });
 }
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+startServer();
