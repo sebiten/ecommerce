070a44041e5e2a1bc3944360ac0644949129f135
Sebiten <sebdevspace@gmail.com>
2023-05-23 17:26:28 -0300

    vite

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+// vite.config.js (dentro de la carpeta client)
+const path = require('path');
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 export default defineConfig({
+  root: path.resolve(__dirname),
+  build: {
+    outDir: path.resolve(__dirname, 'dist'),
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/main.js'),
+      },
+    },
+  },
   server: {
     proxy: {
       '/api': {
@@ -10,8 +22,6 @@ export default defineConfig({
         secure: false,
       },
     },
-    
   },
-  plugins: [reactRefresh()]
-  // some other configuration
-})
+  plugins: [reactRefresh()],
+});
